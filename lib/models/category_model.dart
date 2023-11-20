9d8c4320b8948e06b6e71e96d1fd7b4d3cc98397
Declan <hung34atpvti@gmail.com>
2023-11-20 13:58:32 +0700

    category carousel

---
+++ lib/models/category_model.dart
@@ -0,0 +1,39 @@
+import 'package:equatable/equatable.dart';
+
+class Category extends Equatable {
+  final String name;
+  final String imageUrl;
+
+  const Category({required this.name, required this.imageUrl});
+
+  @override
+  List<Object?> get props => [name, imageUrl];
+
+  static List<Category> categories = [
+    Category(
+      name: 'Rubbers',
+      imageUrl:
+          'https://www.butterfly-global.com/en/products/item/06070_01.jpg',
+    ),
+    Category(
+      name: 'Blades',
+      imageUrl:
+          'https://www.butterfly-global.com/en/products/item/35861_01.jpg',
+    ),
+    Category(
+      name: 'Care',
+      imageUrl:
+          'https://www.butterfly-global.com/en/products/item/74200_01.jpg',
+    ),
+    Category(
+      name: 'Shoes',
+      imageUrl:
+          'https://www.butterfly-global.com/en/products/item/93620_01.jpg',
+    ),
+    Category(
+      name: 'Balls',
+      imageUrl:
+          'https://www.butterfly-global.com/en/products/item/95840_01.jpg',
+    ),
+  ];
+}
--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,4 +1,6 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_shop_app/models/models.dart';
 import 'package:flutter_shop_app/widgets/widgets.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -18,6 +20,20 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       appBar: CustomAppBar(title: 'Ping Pong'),
       bottomNavigationBar: CustomNavBar(),
+      body: Container(
+          child: CarouselSlider(
+        options: CarouselOptions(
+          aspectRatio: 1.5,
+          viewportFraction: 0.9,
+          enlargeCenterPage: true,
+          enlargeStrategy: CenterPageEnlargeStrategy.height,
+        ),
+        items: Category.categories
+            .map((category) => HereCarouselCard(category: category))
+            .toList(),
+      )),
     );
   }
 }
+
+
