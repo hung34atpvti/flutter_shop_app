1a5449a0de6d612b9a8c5169ad9ed1fb9b7cc2ec
Declan <hung34atpvti@gmail.com>
2023-11-21 16:00:57 +0700

    catalog screen: display product

--- lib/screens/catalog/catalog_screen.dart
+++ lib/screens/catalog/catalog_screen.dart
@@ -1,23 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_shop_app/models/models.dart';
 import 'package:flutter_shop_app/widgets/widgets.dart';
 
 class CatalogScreen extends StatelessWidget {
-  const CatalogScreen({super.key});
+  final Category category;
+
+  const CatalogScreen({
+    super.key,
+    required this.category,
+  });
 
   static const String routeName = '/catalog';
 
-  static Route route() {
+  static Route route({required Category category}) {
     return MaterialPageRoute(
       settings: RouteSettings(name: routeName),
-      builder: (_) => CatalogScreen(),
+      builder: (_) => CatalogScreen(category: category),
     );
   }
 
   @override
   Widget build(BuildContext context) {
+    final List<Product> categoryProducts = Product.products
+        .where((product) => product.category == category.name)
+        .toList();
+
     return Scaffold(
-      appBar: CustomAppBar(title: 'Ping Pong'),
+      appBar: CustomAppBar(title: category.name),
       bottomNavigationBar: CustomNavBar(),
+      body: GridView.builder(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 8.0,
+          vertical: 16.0,
+        ),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 1.15,
+        ),
+        itemCount: categoryProducts.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Center(
+            child: ProductCard(
+              product: categoryProducts[index],
+              widthFactor: 2.2,
+            ),
+          );
+        },
+      ),
     );
   }
 }
--- lib/widgets/product_card.dart
+++ lib/widgets/product_card.dart
@@ -3,10 +3,12 @@ import 'package:flutter_shop_app/models/models.dart';
 
 class ProductCard extends StatelessWidget {
   final Product product;
+  final double widthFactor;
 
   const ProductCard({
     super.key,
     required this.product,
+    this.widthFactor = 2.5,
   });
 
   @override
@@ -14,7 +16,7 @@ class ProductCard extends StatelessWidget {
     return Stack(
       children: [
         Container(
-          width: MediaQuery.of(context).size.width / 2.5,
+          width: MediaQuery.of(context).size.width / widthFactor,
           height: 150,
           child: Image.network(
             product.imageUrl,
@@ -24,7 +26,7 @@ class ProductCard extends StatelessWidget {
         Positioned(
           top: 60,
           child: Container(
-            width: MediaQuery.of(context).size.width / 2.5,
+            width: MediaQuery.of(context).size.width / widthFactor,
             height: 80,
             decoration: BoxDecoration(
               color: Colors.black.withAlpha(50),
@@ -35,7 +37,7 @@ class ProductCard extends StatelessWidget {
           top: 65,
           left: 5,
           child: Container(
-            width: MediaQuery.of(context).size.width / 2.5 - 10,
+            width: MediaQuery.of(context).size.width / widthFactor - 10,
             height: 70,
             decoration: BoxDecoration(
               color: Colors.black,
